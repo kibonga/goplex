4254b5956aa5b32901b32d9510f5fa331d230567
Pavle Durdic <pavle.durdic.ext@infront.co>
2024-08-03 00:20:20 +0200

    added request parsing for create handler

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
+	"io"
 	"net/http"
 	"time"
 
@@ -29,6 +32,42 @@ func (app *app) showMovieHandler(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+type MovieCreateRequest struct {
+	Title   string   `json:"title"`
+	Year    int32    `json:"year"`
+	Runtime int32    `json:"runtime"`
+	Genres  []string `json:"genres"`
+}
+
 func (app *app) createMovieHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello from create movie handler"))
+	var movie MovieCreateRequest
+	err := json.NewDecoder(r.Body).Decode(&movie)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+	}
+
+	fmt.Printf("%v", movie)
+	app.writeJson(w, http.StatusCreated, "created successfully", nil)
+
+	defer r.Body.Close()
+}
+
+func (app *app) createMovieHandlerJsonMarshal(w http.ResponseWriter, r *http.Request) {
+	b, err := io.ReadAll(r.Body)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+		return
+	}
+
+	movie := &MovieCreateRequest{}
+	err = json.Unmarshal(b, movie)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+		return
+	}
+
+	defer r.Body.Close()
+
+	fmt.Printf("%v", movie)
+	app.writeJson(w, http.StatusCreated, "created successfully", nil)
 }
