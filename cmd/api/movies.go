81572a122aedbecd613ec2056f6392713c96a66a
Pavle Durdic <pavle.durdic.ext@infront.co>
2024-08-04 12:26:34 +0200

    added custom json encdoing for runtime field

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 	"time"
 
@@ -30,29 +31,33 @@ func (app *app) showMovieHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 type MovieCreateRequest struct {
-	Title   string   `json:"title"`
-	Year    int32    `json:"year"`
-	Runtime int32    `json:"runtime"`
-	Genres  []string `json:"genres"`
+	Title   string       `json:"title"`
+	Year    int32        `json:"year"`
+	Runtime data.Runtime `json:"runtime"`
+	Genres  []string     `json:"genres"`
 }
 
 func (app *app) createMovieHandler(w http.ResponseWriter, r *http.Request) {
 	var movie MovieCreateRequest
 
 	err := app.decodeJson(r, &movie)
+	defer r.Body.Close()
 	if err != nil {
 		app.badRequestResponse(w, r, err)
+		return
 	}
 
-	defer r.Body.Close()
+	fmt.Printf("Movie: %+v \n", movie)
+	fmt.Fprintf(w, "Movie: %+v \n", movie)
 }
 
 func (app *app) createMovieHandlerMarshal(w http.ResponseWriter, r *http.Request) {
 	var movie MovieCreateRequest
 	err := app.unmarshalJson(r, &movie)
+	defer r.Body.Close()
 	if err != nil {
 		app.badRequestResponse(w, r, err)
+		return
 	}
 
-	defer r.Body.Close()
 }
