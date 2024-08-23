0bea5ac12df2fec5613994b7cbf466838a07df26
Pavle Durdic <pavle.durdic.ext@infront.co>
2024-08-23 23:19:49 +0200

    added graceful shutdown of background tasks

--- cmd/api/helpers.go
+++ cmd/api/helpers.go
@@ -175,7 +175,11 @@ func (app *app) unmarshalJson(r *http.Request, data interface{}) error {
 }
 
 func (app *app) background(fn func()) {
+	app.wg.Add(1)
+
 	go func() {
+
+		defer app.wg.Done()
 
 		defer func() {
 			if err := recover(); err != nil {
