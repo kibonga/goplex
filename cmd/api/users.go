2c4d7ba130d9a8ecd856cb5a84af56ad767847af
Pavle Durdic <pavle.durdic.ext@infront.co>
2024-08-25 21:24:49 +0200

    setting up db table and creating token

--- cmd/api/users.go
+++ cmd/api/users.go
@@ -80,3 +80,11 @@ func (app *app) fooHandler(w http.ResponseWriter, r *http.Request) {
 
 	app.writeJson(w, http.StatusOK, payload{"foo": "bar"}, nil)
 }
+
+func (app *app) tokenHandler(w http.ResponseWriter, r *http.Request) {
+	userID := 6
+	ttl := time.Duration(time.Second * 300)
+	scope := data.ScopeActivation
+
+	app.models.Tokens.New(int64(userID), ttl, scope)
+}
